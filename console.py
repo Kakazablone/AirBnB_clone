dc91a5bf27416c77c379bd26054d1385d71fbf89
Zablon Samba <zablonsamba@gmail.com>
2023-12-04 21:03:58 -0800

    Set up Console Foundations

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Command interpreter adapted from Cmd Module"""
+import models
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """The command line interpreter"""
+
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """ Exit at EOF"""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the interpreter"""
+        return True
+
+    def emptyline(self):
+        """Do nothing if line is empty"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
